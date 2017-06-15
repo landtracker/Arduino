c0eef1d5e00c6f7cec4e5898641cdd31911eb4b8
Vinícuis S. Diaz <diaz@alunos.utfpr.edu.br>
2017-06-15 20:37:09 -0300

    Comit do Arduino

---
+++ Pinagem.h
@@ -0,0 +1,17 @@
+#ifndef PINAGEM_H
+#define PINAGEM_H
+//--CONSTANTES DE SISTEMA
+
+//Pinos do Sonar e do Ultrassom
+
+  const int SERVOPIN_01 = 9;
+  const int SERVOPIN_02 = 10;
+  const int SERVOPIN_03 = 11;
+
+
+
+
+ 
+  #endif //-- Defini��es
+
+
